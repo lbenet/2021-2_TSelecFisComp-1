49fcf8304d97fd87616085196986dea38b8845e2
Erasmo98 <erasmo@ciencias.unam.mx>
2021-07-19 01:53:20 -0500

    Solución del tercer problema de la segunda tarea

--- tareas/Erasmo/Tarea2.jl
+++ tareas/Erasmo/Tarea2.jl
@@ -17,17 +17,20 @@ Qc(x,c) = x^2 + c
 Qc(x, args...) = Qc(x, args[1])
 
 """
-    Qcⁿ(x, args)
+    fⁿ(f, x, args)
 
-Esta función regresa el n-enésimo iterado de ``Q_c(x,c)``,
+Esta función regresa el n-enésimo iterado de ``f(x,c)``,
 donde el valor a iterar es `x` y el valor del parámetro
 es `c`, donde `c = args[1]` y `n = args[2]`.
+
+La función `f` debe ser definida de tal manera que `f(x0, c)`
+tenga sentido.
 """
-function Qcⁿ(x, args)
+function fⁿ(f, x, args)
     c, n = args[1], args[2]
-    n <= 1 && return Qc(x,c)
+    n <= 1 && return f(x,c)
     for it = 1:n
-        x = Qc(x,c)
+        x = f(x,c)
     end
     return x
 end
@@ -116,10 +119,10 @@ plot(xrange, y, xlabel = 'c', ylabel = "q^2n(c)", label = permutedims(labels),
 # Además, la gráfica sugiere que todas las funciones tienen las mismas
 # raíces. Esto se va a demostrar en el siguiente párrafo.
 # 
-# Sea $\alpha_n^*$ una raíz de $q^n$, es deicr, tal $q^n(\alpha_n^*) = 0$.
-# Nótese que $\alpha_n^*$ también es raíz de $q^{2n}$:
+# Sea $\beta_n^*$ una raíz de $q^n$, es deicr, tal $q^n(\beta_n^*) = 0$.
+# Nótese que $\beta_n^*$ también es raíz de $q^{2n}$:
 # 
-# $$ q^{2n}(\alpha_n^*) = Q_{\alpha_n^*}^{2n}(0) = Q_{\alpha_n^*}^n\big(Q_{\alpha_n^*}^n(0)\big) = Q_{\alpha_n^*}^n\big(q^n(\alpha_n^*)\big) = Q_{\alpha_n^*}^n(0) = q^n(\alpha_n^*) = 0, $$
+# $$ q^{2n}(\beta_n^*) = Q_{\beta_n^*}^{2n}(0) = Q_{\beta_n^*}^n\big(Q_{\beta_n^*}^n(0)\big) = Q_{\beta_n^*}^n\big(q^n(\beta_n^*)\big) = Q_{\beta_n^*}^n(0) = q^n(\beta_n^*) = 0, $$
 # 
 # por lo que toda raíz de $q^n$ es también raíz de $q^{2n}$ y esto es cierto
 # para toda $n \in \mathbb{N}$. Esto quiere decir que una forma de calcular los
@@ -164,19 +167,34 @@ roots(f, X; contractor, tol) = roots(f, X, contractor, tol)
 #-
 
 """
-cₙ(f, cₙ₋₁::Root, n, interval; kwargs...)
+cₙ(f, cₙ₋₁::Root, n, interval, p; kwargs...)
 
 Regresa el valor cₙ del parámetro c tal que
-f tiene un ciclo superestable de periodo
-2ⁿ.
+`p` pertenece al ciclo superestable de periodo
+2ⁿ del mapeo `f`.
+
+La función `f` debe ser definida de tal manera
+que `f(x0, c)` tenga sentido.
 
 Esto se hace calculando raíces en el
 intervalo `interval`. Los `kwargs`
 se pasan como argumentos a 
 `IntervalRootFinding.root`.
+
+Se regresa la raíz más cercana a la izquierda
+de `cₙ₋₁` si `direccion = :izquierda` o la más
+cercana a la derecha de `cₙ₋₁` si
+`direccion = :derecha`.
 """
-cₙ(f, cₙ₋₁::Root, n, interval; kwargs...) = maximum(filter(x->!issameroot(x, cₙ₋₁),
-        roots(c->f(0.0, (c, 2^n)), interval; kwargs...) ))
+function cₙ(f, cₙ₋₁::Root, n, p, interval, direccion; kwargs...)
+    if direccion == :izquierda
+        F = maximum
+    elseif direccion == :derecha
+        F = minimum
+    end
+    
+    F(filter(x->!issameroot(x, cₙ₋₁), roots(c->fⁿ(f, p, (c, 2^n))-p, interval; kwargs...) ))
+end
 #-
 
 """
@@ -184,37 +202,58 @@ intervalo(cₙ₋₂interval, cₙ₋₁interval)
 
 Calcula el intervalo donde se buscará cn.
 """
-function intervalo(cₙ₋₂interval, cₙ₋₁interval)
-    d = cₙ₋₂interval.hi-cₙ₋₁interval.lo
-    (cₙ₋₁interval.lo - 0.9d)..(cₙ₋₁interval.hi + 0.1d)
+function intervalo(cₙ₋₂interval, cₙ₋₁interval, direccion)
+    if direccion == :izquierda
+        d = cₙ₋₂interval.hi-cₙ₋₁interval.lo
+        I = (cₙ₋₁interval.lo - 0.9d)..(cₙ₋₁interval.hi + 0.1d)
+    elseif direccion == :derecha
+        d = cₙ₋₁interval.hi-cₙ₋₂interval.lo
+        I = (cₙ₋₁interval.lo - 0.1d)..(cₙ₋₁interval.hi + 0.9d)
+    end
+    
+    return I
 end
 
-intervalo(cₙ₋₂::Root, cₙ₋₁::Root) = intervalo(cₙ₋₂.interval, cₙ₋₁.interval)
+intervalo(cₙ₋₂::Root, cₙ₋₁::Root, direccion) = intervalo(cₙ₋₂.interval, cₙ₋₁.interval, direccion)
 #-
 
-cₙ(f, cₙ₋₂::Root, cₙ₋₁::Root, n; kwargs...) = cₙ(f, cₙ₋₁, n, intervalo(cₙ₋₂, cₙ₋₁); kwargs...)
-
-function cₙ!(f, cns::AbstractVector, i, n; kwargs...)
-    cns[i] = cₙ(f, cns[i-2], cns[i-1], n; kwargs...)
+function direccion(cₙ₋₂interval, cₙ₋₁interval)
+    if cₙ₋₁interval < cₙ₋₂interval
+        return :izquierda
+    elseif cₙ₋₂interval < cₙ₋₁interval
+        return :derecha
+    end
 end
 
-cₙ!(f, cns::AbstractVector, n; kwargs...) = cₙ!(f, cns, n, n; kwargs...)
+direccion(cₙ₋₂::Root, cₙ₋₁::Root) = direccion(cₙ₋₂.interval, cₙ₋₁.interval)
+#-
 
-function cₙ!(f, cns::AbstractVector, i, n₁, n₂; kwargs...)
+function cₙ(f, cₙ₋₂::Root, cₙ₋₁::Root, n, p; kwargs...)
+    D = direccion(cₙ₋₂, cₙ₋₁)
+    cₙ(f, cₙ₋₁, n, p, intervalo(cₙ₋₂, cₙ₋₁, D), D; kwargs...)
+end
+
+function cₙ!(f, cns::AbstractVector, i, n, p; kwargs...)
+    cns[i] = cₙ(f, cns[i-2], cns[i-1], n, p; kwargs...)
+end
+
+cₙ!(f, cns::AbstractVector, n, p; kwargs...) = cₙ!(f, cns, n, n, p; kwargs...)
+
+function cₙ!(f, cns::AbstractVector, i, n₁, n₂, p; kwargs...)
     for (j, n) ∈ enumerate(n₁:n₂)
-        cₙ!(f, cns, i+j-1, n; kwargs...)
+        cₙ!(f, cns, i+j-1, n, p; kwargs...)
     end
     return cns
 end
 #-
 
 n = 11
-rootcns = OffsetVector(Vector{Root{Interval{Float64}}}(undef, n+1), 0:n)
-rootcns[0] = Root(0.0..0.0, :unique)
-rootcns[1] = Root((-1)..(-1), :unique)
+Qrootcns = OffsetVector(Vector{Root{Interval{Float64}}}(undef, n+1), 0:n)
+Qrootcns[0] = Root(interval(0.0), :unique)
+Qrootcns[1] = Root(interval(-1), :unique)
 
 setformat(:midpoint)
-cₙ!(Qcⁿ, rootcns, 2, 2, 11, contractor=Krawczyk, tol=1e-16)
+cₙ!(Qc, Qrootcns, 2, 2, n, 0.0, contractor=Krawczyk, tol=1e-16)
 #-
 
 """
@@ -250,20 +289,19 @@ end
 fₙ!(fns, cns) = fₙ!(fns, cns, firstindex(fns), firstindex(cns), length(fns))
 #-
 
-cns = OffsetVector(Vector{Interval{Float64}}(undef, n+1), 0:n)
-@. cns = interval(rootcns)
-fns = OffsetVector(Vector{Interval{Float64}}(undef, n-1), 0:n-2)
-fₙ!(fns, cns)
+Qcns = interval.(Qrootcns)
+Qfns = OffsetVector(Vector{Interval{Float64}}(undef, n-1), 0:n-2)
+fₙ!(Qfns, Qcns)
 #-
 
 # ## $\delta$
 
-println("c$n ≈ c∞ ≈ $(cns[n])")
-println("f∞ = δ ≈ $(fns[n-2])")
+println("c$n ≈ c∞ ≈ $(Qcns[n])")
+println("f∞ = δ ≈ $(Qfns[n-2])")
 #-
 
 """
-cₙ(f, cₙ, n)
+dₙ(f, cₙ, n)
 
 Calcula el valor de dₙ. Es decir, la
 distancia mínima distinta de cero del
@@ -275,30 +313,30 @@ En este caso `f` es el mapeo y
 el cual ocurre el periodo superestabl
 de periodo 2ⁿ.
 """
-function dₙ(f, cₙ, n)
-    x = f(zero(cₙ), (cₙ, 1))
-    d = abs(x)
+function dₙ(f, cₙ, n, p)
+    x = f(p, cₙ)
+    d = abs(x-p)
     
     for i ∈ 1:2^n-2
-        x = f(x, (cₙ, 1))
-        abs(x) < d && (d = abs(x))
+        x = f(x, cₙ)
+        abs(x-p) < d && (d = abs(x-p))
     end
     
     return d
 end
 
-function dₙ!(f, dns, cns, i, j, n₁, n₂)
+function dₙ!(f, dns, cns, i, j, n₁, n₂, p)
     for (k, n) ∈ enumerate(n₁:n₂)
-        dns[i+k-1] = dₙ(f, cns[j+k-1], n)
+        dns[i+k-1] = dₙ(f, cns[j+k-1], n, p)
     end
     return dns
 end
 
-dₙ!(f, dns, cns) = dₙ!(f, dns, cns, firstindex(dns), firstindex(cns)+1, 1, length(dns))
+dₙ!(f, dns, cns, p) = dₙ!(f, dns, cns, firstindex(dns), firstindex(cns)+1, 1, length(dns), p)
 #-
 
-dns = Vector{Interval{Float64}}(undef, n)
-dₙ!(Qcⁿ, dns, cns)
+Qdns = Vector{Interval{Float64}}(undef, n)
+dₙ!(Qc, Qdns, Qcns, 0.0)
 #-
 
 """
@@ -328,13 +366,13 @@ end
 αₙ!(αns, dns) = αₙ!(αns, dns, firstindex(αns), firstindex(dns), length(αns))
 #-
 
-αns = Vector{Interval{Float64}}(undef, n-1)
-αₙ!(αns, dns)
+Qαns = Vector{Interval{Float64}}(undef, n-1)
+αₙ!(Qαns, Qdns)
 #-
 
 # ## $\alpha$
 
-println("α ≈ $(αns[n-1])")
+println("α ≈ $(Qαns[n-1])")
 #-
 
 # ## Ejercicio 3:
@@ -347,5 +385,144 @@ println("α ≈ $(αns[n-1])")
 # 
 # - ¿Qué interpretación le pueden dar al resultado?
 
-#Respuestas
+# ## Solución:
+# 
+# Los puntos más alejados a las bifurcaciones son tales que la
+# derivada del mapeo son cero para cualquier valor del parámetro
+# $c$. En este caso se tiene que
+# 
+# $$ S_c'(x_k) = c \cos(x_k) = 0 \quad \Rightarrow \quad x_k = \frac{2k+1}{2}\pi,\quad k \in \mathbb{Z} .$$
+# 
+# Un punto en el que la derivada de $S_c$ se anula para culaquier
+# valor de $c$ es $\pi/2$. El estudio de este mapeo se hará alrededor
+# de este punto. Es más, para que el intervalo de valores de $x$ se
+# mapee en sí mismo, se tomarán valores de $x$ y $c$ tales que
+# $x,\, c \in [0,\, \pi]$.
+# 
+# A continuación se usa el código de la clase _32-Bifurcaciones_
+# para graficar el diagrama de bifurcaciones de $S_c$.
+
+Sc(x, c) = c*sin(x)
+Sc(x, args...) = Sc(x, args[1])
 #-
+
+"""
+    ciclosestables!(xx, f, cc, nit, nout)
+
+Esta función itera el mapeo `f`, de una variable, `nit+nout` veces,
+usando como condición inicial `x0`; los últimos `nout` iterados
+actualizan al vector `xx` que tiene longitud `nout`. `cc` es el valor
+del parámetro del mapeo `f`. El mapeo `f` debe ser definido de
+tal manera que `f(x0, cc)` tenga sentido. La idea es que los últimos
+`nout` iterados reflejen los ciclos estables del mapeo `f`.
+"""
+function ciclosestables!(xx, f, x0, cc, nit, nout)
+    @assert (nit > 0) && (nout > 0)
+
+    #Primeros nit iterados
+    for it = 1:nit
+        x0 = f(x0, cc)
+    end
+
+    #Se guardan los siguientes nout iterados
+    for it = 1:nout
+        x0 = f(x0, cc)
+        @inbounds xx[it] = x0
+    end
+
+    nothing
+end
+
+"""
+    diag_bifurc(f, nit, nout, crange)
+
+Itera el mapeo `f` `nit+nout` veces y regresa una matriz
+cuya columna `i` tiene los últimos `nout` iterados del mapeo
+para el valor del parámetro del mapeo `crange[i]`.
+
+La función `f` debe ser definida de tal manera que `f(x0, c)`
+tenga sentido.
+"""
+function diag_bifurc(f, x0, nit, nout, crange)
+    #xx = Vector{Float64}(nout)
+    ff = Array{Float64}(undef, (nout, length(crange)))
+
+    for ic in eachindex(crange)
+        c = crange[ic]
+        ciclosestables!(view(ff, :, ic), f, x0, c, nit, nout)
+        #ff[:,ic] = xx
+    end
+
+    return ff
+end
+#-
+
+crange = 0:1/2^10:π
+
+ff = diag_bifurc(Sc, π/2, 2000, 256, crange);
+cc = ones(size(ff, 1)) * crange';
+
+#Lo siguiente cambia las matrices en vectores; ayuda un poco para los dibujos
+ff = reshape(ff, size(ff, 1)*size(ff, 2));
+cc = reshape(cc, size(ff));
+#-
+
+scatter(cc, ff, markersize=0.5, markerstrokestyle=:solid,
+    legend=false, title="Fig. 1")
+plot!([0,3.1416], [π/2,π/2], color=:red)
+xaxis!("c")
+yaxis!("x_infty")
+savefig("diag_bif.png")
+#-
+
+# ![Fig 1](diag_bif.png "Fig. 1")
+# 
+# De la Fig. 1 nótese que para $c < 1$, $x_\infty = 0$.
+# Esto es claro ya que $S_c(x) < 1$ para cualquier valor
+# de $x$, de tal forma que $x_n$ va disminuyendo conforme
+# se itera.
+# 
+# También nótese que el primer ciclo superestable ocurre
+# en $\pi/2$:
+# 
+# $$ S_{c_0}(\pi/2) = c_0 \sin(\pi/2) = c_0 = \pi/2 .$$
+# 
+# Éste es el único valor de $c_n$ que puede calcularse
+# analíticamente, sin embargo de la Fig.1 se nota que
+# $c_1$ está cerca de $2.5$ y $c_2$ cerca de $2.7$.
+
+Srootcns = OffsetVector(Vector{Root{Interval{Float64}}}(undef, n+1), 0:n)
+Srootcns[0] = Root(interval(π/2), :unique)
+Srootcns[1] = cₙ(Sc, Srootcns[0], 1, π/2, 2.4..2.6, :derecha)
+
+setformat(:midpoint)
+cₙ!(Sc, Srootcns, 2, 2, n, π/2, contractor=Krawczyk, tol=1e-16);
+#-
+
+Scns = interval.(Srootcns)
+Sfns = OffsetVector(Vector{Interval{Float64}}(undef, n-1), 0:n-2)
+fₙ!(Sfns, Scns)
+#-
+
+# ## $\delta$
+
+println("c$n ≈ c∞ ≈ $(Scns[n])")
+println("f∞ = δ ≈ $(Sfns[n-2])")
+#-
+
+Sdns = Vector{Interval{Float64}}(undef, n)
+Sαns = Vector{Interval{Float64}}(undef, n-1)
+dₙ!(Sc, Sdns, Scns, π/2)
+αₙ!(Sαns, Sdns)
+#-
+
+# ## $\alpha$
+
+println("α ≈ $(Sαns[n-1])")
+#-
+
+# Para $S_c$ obtienen los mismos valores de $\delta$
+# y $\alpha$ que para $Q_c$. Esto se debe a que
+# localmente $S_c(\pi/2)$ y $Q_c(0)$ se parecen.
+# Es decir, porque ambas funciones tienen extremos
+# cuadráticos.
